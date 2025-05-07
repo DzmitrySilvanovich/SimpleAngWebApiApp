f5eb513c809fafa8fd3e8cc9b035ac30aa54e3d2
Dzmitry <dzmitry_silvanovich@epam.com>
2025-05-07 16:50:42 +0300

    Added docker files

--- WebApiAuthCRUD/WebApiAuthCRUD/Program.cs
+++ WebApiAuthCRUD/WebApiAuthCRUD/Program.cs
@@ -4,15 +4,24 @@ using WebApiAuthCRUD.BAL.Services;
 using WebApiAuthCRUD.DAL.Contracts;
 using WebApiAuthCRUD.DAL.Repositories;
 using Microsoft.Identity.Web;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-builder.Services.AddDbContext<ApplicationContext>(
-    options => SqlServerDbContextOptionsExtensions.UseSqlServer(options, connectionString));
+//connectionString = "";
+//var connectionString = builder.Configuration.GetConnectionString("Default") ?? "Host = 172.17.0.3:5432; Database = simple_postgres_instance; Username = postgres; Password = 4Vd59Ab4dt; Pooling = true;";
+var connectionString = builder.Configuration.GetConnectionString("OrWSLDockerPostGresConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//?? "Host = localhost:5432; Database = simple_postgres_instance; Username = postgres; Password = Password12345; Pooling = true;";
+
+//builder.Services.AddDbContext<ApplicationContext>(
+//    options => SqlServerDbContextOptionsExtensions.UseSqlServer(options, connectionString));
+
+builder.Services.AddDbContext<ApplicationContext>(opt =>
+            opt.UseNpgsql(connectionString));
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
